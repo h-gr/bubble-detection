46decdd620723fb33ec06f7716ef738bbe85f686
unknown <hugoalexgr@gmail.com>
2020-05-26 15:46:09 +0200

    config data paths for bubble

--- mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-bubble.py
+++ mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-bubble.py
@@ -1,13 +1,13 @@
 _base_ = './faster_rcnn_r50_fpn_1x_coco.py'
-data_root = 'data/coco/'
+data_root = 'data/'
 classes = ('bubble', )
 data = dict(
     train=dict(classes=classes,
-    ann_file=data_root + 'annotations/instances_train2017.json',
-    img_prefix=data_root + 'train2017/',),
+    ann_file=data_root + 'train/_annotations.coco.json',
+    img_prefix=data_root + 'train/',),
     val=dict(classes=classes,
-    ann_file=data_root + 'annotations/instances_train2017.json',
-    img_prefix=data_root + 'train2017/',),
+    ann_file=data_root + 'valid/_annotations.coco.json',
+    img_prefix=data_root + 'valid/',),
     test=dict(classes=classes,
-    ann_file=data_root + 'annotations/instances_train2017.json',
-    img_prefix=data_root + 'train2017/',))
+    ann_file=data_root + 'test/_annotations.coco.json',
+    img_prefix=data_root + 'test/',))
